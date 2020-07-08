10067b10a00b836c3e07b8ccc3432a80caf7a128
YinjianChen <YinjianChen02@gmail.com>
2020-07-08 21:39:55 +0800

    index function

--- Go/Demo/GitChat/main.go
+++ Go/Demo/GitChat/main.go
@@ -1,13 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"html/template"
+)
+
 
 func handler(writer http.ResponseWriter,request *http.Request){
 	print("haha")
 }
 
+func index(writer http.ResponseWriter,request *http.Request){
+	files := []string{"templates/layout.html","templates/navbar.html","templates/index.html"}
+	templates := template.Must(template.ParseFiles(files...))
+	threads, err := data.Threads();if err == nil {
+		templates.ExecuteTemplate(w,"layout",threads)
+	}
+	print("response")
+}
+
 func main() {
-	http.HandleFunc("/",handler)
-	http.ListenAndServe(":8080",nil)
-	print("j")
+
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/",http.StripPrefix("/static/",files))
+	mux.HandleFunc("/",index)
+
+	server := &http.Server{
+		Addr: "0.0.0.0:8000",
+		Handler:mux,
+	}
+
+
+	server.ListenAndServe()
 }
