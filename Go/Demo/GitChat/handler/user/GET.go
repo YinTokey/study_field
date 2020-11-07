0abf537a7b09cfad1076ec92f8f43e4a045a41fe
YinjianChen <YinjianChen02@gmail.com>
2020-11-07 17:13:18 +0800

    user get

--- Go/Demo/GitChat/handler/user/GET.go
+++ Go/Demo/GitChat/handler/user/GET.go
@@ -1,1 +1,16 @@
 package user
+
+import (
+	. "gitchat/handler"
+)
+
+func Get(c *gin.Context) {
+	username := c.Param("username")
+	user, err := model.GetUser(username)
+	if err != nil {
+		SendResponse(c, errno.ErrUserNotFound, nil)
+		return
+	}
+
+	SendResponse(c, nil, user)
+}
