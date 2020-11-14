66faa0c4e56abe04ed519cfcb52200187a93e6df
YinjianChen <YinjianChen02@gmail.com>
2020-11-14 22:40:28 +0800

    user service

--- Go/Demo/lottery/web/controllers/index.go
+++ Go/Demo/lottery/web/controllers/index.go
@@ -2,14 +2,44 @@ package controllers
 
 import (
 	"github.com/kataras/iris/v12"
+	"lottery/services"
 )
 
 type IndexController struct {
 	Ctx iris.Context
+	ServiceUser services.UserService
 
 }
 
 func (c *IndexController) Get() string {
 	c.Ctx.Header("Content-Type", "text/html")
 	return "welcome to Go抽奖系统，<a href='/public/index.html'>开始抽奖</a>"
+}
+
+// 登录 GET /login
+func (c *IndexController) GetLogin() {
+	// 每次随机生成一个登录用户信息
+	uid := comm.Random(100000)
+	loginuser := models.ObjLoginuser{
+		Uid:      uid,
+		Username: fmt.Sprintf("admin-%d", uid),
+		Now:      comm.NowUnix(),
+		Ip:       comm.ClientIP(c.Ctx.Request()),
+	}
+	refer := c.Ctx.GetHeader("Referer")
+	if refer == "" {
+		refer = "/public/index.html?from=login"
+	}
+	comm.SetLoginuser(c.Ctx.ResponseWriter(), &loginuser)
+	comm.Redirect(c.Ctx.ResponseWriter(), refer)
+}
+
+// 退出 GET /logout
+func (c *IndexController) GetLogout() {
+	refer := c.Ctx.GetHeader("Referer")
+	if refer == "" {
+		refer = "/public/index.html?from=logout"
+	}
+	comm.SetLoginuser(c.Ctx.ResponseWriter(), nil)
+	comm.Redirect(c.Ctx.ResponseWriter(), refer)
 }
