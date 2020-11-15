0945f15c55e025527a4f78b22ab0b37e56b81d2a
YinjianChen <YinjianChen02@gmail.com>
2020-11-15 10:31:40 +0800

    other daos

--- Go/Demo/lottery/web/controllers/index.go
+++ Go/Demo/lottery/web/controllers/index.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/kataras/iris/v12"
+	"lottery/comm"
+	"lottery/models"
 	"lottery/services"
 )
 
@@ -11,10 +14,33 @@ type IndexController struct {
 
 }
 
+
 func (c *IndexController) Get() string {
 	c.Ctx.Header("Content-Type", "text/html")
 	return "welcome to Go抽奖系统，<a href='/public/index.html'>开始抽奖</a>"
 }
+
+// http://localhost:8080/myprize
+//func (c *IndexController) GetMyprize() map[string]interface{} {
+//	rs := make(map[string]interface{})
+//	rs["code"] = 0
+//	rs["msg"] = ""
+//	// 验证登录
+//	loginuser := comm.GetLoginUser(c.Ctx.Request())
+//	if loginuser == nil || loginuser.Uid < 1 {
+//		rs["code"] = 101
+//		rs["msg"] = "请先登录，再来抽奖"
+//		return rs
+//	}
+//	// 只读取出来最新的100次中奖记录
+//	list := c.ServiceResult.SearchByUser(loginuser.Uid, 1, 100)
+//	rs["prize_list"] = list
+//	// 今天抽奖次数
+//	day, _ := strconv.Atoi(comm.FormatFromUnixTimeShort(time.Now().Unix()))
+//	num := c.ServiceUserday.Count(loginuser.Uid, day)
+//	rs["prize_num"] = conf.UserPrizeMax - num
+//	return rs
+//}
 
 // 登录 GET /login
 func (c *IndexController) GetLogin() {
