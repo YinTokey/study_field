a28ea13ac49cd8ba6741d351535bd5825c8ffc49
YinjianChen <YinjianChen02@gmail.com>
2020-12-01 22:53:10 +0800

    query from context

--- Go/Demo/campus/Week02/api/picture_api.go
+++ Go/Demo/campus/Week02/api/picture_api.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 type Response struct {
@@ -17,13 +18,18 @@ func QueryPicture(c *gin.Context) {
 
 	// 已知表中存有一条记录
 ///	pic := &models.Picture{URL: "https://wwww.baidu.com", UserID: "123", Author: "JACK"}
-	id := 123
+
+	id, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		c.JSON(404, "invalid id")
+		return
+	}
 
 	service := services.NewPictureService()
-	err := c.ShouldBind(&service)
+	err = c.ShouldBind(&service)
 	if err != nil {
 		c.JSON(500, "server error")
-		return;
+		return
 	}
 	res, err := service.Query(id)
 
