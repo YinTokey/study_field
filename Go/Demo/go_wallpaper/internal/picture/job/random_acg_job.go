4e1415e59fb0199839d485921429d6a3bebc4406
YinjianChen <YinjianChen02@gmail.com>
2021-01-21 09:10:52 +0800

    get valid image w h

--- Go/Demo/go_wallpaper/internal/picture/job/random_acg_job.go
+++ Go/Demo/go_wallpaper/internal/picture/job/random_acg_job.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/disintegration/imageorient"
 	"image"
+	_ "image/jpeg"
+	_ "image/png"
 	"net/http"
 )
 
@@ -19,9 +21,22 @@ func NewRandomAcgJob() *RandomAcgJob {
 
 func (j *RandomAcgJob) FetchLink_1() {
 
-	location := j.GetReadImageUrl(url_2)
+	validUrl := j.GetRealImageUrl(url_2)
 
-	fmt.Println(location)
+	// 过滤掉长度过段的
+	if len(validUrl) < 35 {
+		return
+	}
+	fmt.Println(validUrl)
+
+	img, err := GetImage(validUrl)
+
+	if err != nil {
+		fmt.Println("get img err ", err)
+	}
+
+	fmt.Println(img.Bounds())
+
 }
 
 // RedirectFunc 重定向禁止
@@ -31,7 +46,7 @@ func RedirectFunc(req *http.Request, via []*http.Request) error {
 	return http.ErrUseLastResponse
 }
 
-func (j *RandomAcgJob) GetReadImageUrl(url string) string {
+func (j *RandomAcgJob) GetRealImageUrl(url string) string {
 	// 禁止重定向
 	client := &http.Client{CheckRedirect: RedirectFunc}
 	rep, err := client.Get(url)
@@ -41,20 +56,23 @@ func (j *RandomAcgJob) GetReadImageUrl(url string) string {
 	defer rep.Body.Close()
 
 	//读取响应的结果
-	localtion := fmt.Sprintf("%s", rep.Header["Location"])
+	localtion := rep.Header["Location"]
 
-	return localtion
+	return localtion[0]
 
 }
 
 func GetImage(url string) (image.Image, error) {
+	fmt.Println("start request ", url)
 	resp, err := http.Get(url)
 	defer resp.Body.Close()
 	if err != nil {
+		fmt.Println("get image request err ", err)
 		return nil, err
 	}
 	img, _, err := imageorient.Decode(resp.Body)
 	if err != nil {
+		fmt.Println("decode image err ", err)
 		return nil, err
 	}
 	return img, nil
