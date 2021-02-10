ba391da79969ca5ac5709b64a8a0d120edcd3734
YinjianChen <YinjianChen02@gmail.com>
2021-02-10 15:53:58 +0800

    account run success

--- Go/wallpaper_group/account_server/conf/conf.go
+++ Go/wallpaper_group/account_server/conf/conf.go
@@ -1,18 +1,30 @@
 package conf
 
 import (
-	"os"
 	"account_server/cache"
 	"account_server/model"
 	"account_server/util"
-
-	"github.com/joho/godotenv"
+	"fmt"
+	"github.com/spf13/viper"
+	"os"
 )
 
 // Init 初始化配置项
 func Init() {
+
+	//  配置文件加载到 viper中
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("conf")
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("account_server") // 读取环境变量的前缀为 go_wallpaper
+
 	// 从本地读取环境变量
-	godotenv.Load()
+	//godotenv.Load()
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("配置文件加载失败", err)
+	}
 
 	// 设置日志级别
 	util.BuildLogger(os.Getenv("LOG_LEVEL"))
@@ -22,7 +34,16 @@ func Init() {
 		util.Log().Panic("翻译文件加载失败", err)
 	}
 
+	dbUsername := viper.Get("db.username")
+	dbPassword := viper.Get("db.password")
+	dbName := viper.Get("db.name")
+	dbAddr := viper.Get("db.addr")
+	dbConfig := viper.Get("db.config")
+	dsn := fmt.Sprintf("%v:%v@tcp(%v)/%v?%v", dbUsername, dbPassword, dbAddr, dbName, dbConfig)
+
+	fmt.Println(dsn)
+
 	// 连接数据库
-	model.Database(os.Getenv("MYSQL_DSN"))
+	model.Database(dsn)
 	cache.Redis()
 }
