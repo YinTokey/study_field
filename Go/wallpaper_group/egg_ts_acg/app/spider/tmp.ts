f7d25690528055f421895e2dc4b9bd4925f6831f
YinjianChen <YinjianChen02@gmail.com>
2021-05-30 16:22:35 +0800

    node event emit

---
+++ Go/wallpaper_group/egg_ts_acg/app/spider/tmp.ts
@@ -0,0 +1,20 @@
+import {EventEmitter} from 'events';
+
+const timerEventEmitter = new EventEmitter();
+
+let currentTime = 0;
+
+setInterval(()=> {
+    currentTime++;
+    timerEventEmitter.emit('update',currentTime);
+}, 1000);
+
+// timerEventEmitter.on('update',time=>{
+//     console.log('从发布者收到的消息：');
+//     console.log(`程序已经运行了 ${time} 秒`);
+// });
+
+timerEventEmitter.once('update', (time) => {
+    console.log('从发布者收到的消息：');
+    console.log(`程序已经运行了 ${time} 秒`);
+});
