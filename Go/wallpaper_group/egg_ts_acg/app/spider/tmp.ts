92a4d8f23c64644c33b1ddd6d30afdf459a77025
YinjianChen <YinjianChen02@gmail.com>
2021-06-06 21:18:13 +0800

    typescript add more

--- Go/wallpaper_group/egg_ts_acg/app/spider/tmp.ts
+++ Go/wallpaper_group/egg_ts_acg/app/spider/tmp.ts
@@ -18,3 +18,9 @@ timerEventEmitter.once('update', (time) => {
     console.log('从发布者收到的消息：');
     console.log(`程序已经运行了 ${time} 秒`);
 });
+
+const arr = [1,2,3];
+function sum(x,y,z) {
+    return x+y+z;
+}
+
